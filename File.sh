8167aaafdc46ea7c8fdc61f680d2c1b79a61cb41
ProLinuxFree <67120508+ProLinuxFree@users.noreply.github.com>
2020-06-26 10:52:06 -0700

    Update File.sh

--- File.sh
+++ File.sh
@@ -6,7 +6,7 @@ if [ ! -z "$Files" ]
    do
    RpmPerm=`/bin/rpm -q --queryformat "[%{FILEMODES:perms} %{FILENAMES}\n]" audit | /bin/egrep "[[:space:]]$File$" | /bin/awk '{print $1}'`
    FilePerm=`/bin/ls -ldL "$File" | /bin/awk '{print $1}' | /bin/sed 's/\.//'`
-   RpmPermRegEx=`/bin/echo "$RpmPerm" | /bin/awk '{gsub(/[^-]/,".",$0) gsub(/^./,".",$0); print}'`
+   RpmPermRegEx=`/bin/echo "$RpmPerm" | /bin/awk '{gsub(/[^-]/,".",$0); gsub(/^./,".",$0); print}'`
    /bin/echo "$FilePerm" | /bin/awk '$0 !~ /^'$RpmPerm RegEx'$/{ print "'$File':\n RPM Permissions: '$RpmPerm'\n File Permissions:'$FilePerm'" }'
 done
 fi
